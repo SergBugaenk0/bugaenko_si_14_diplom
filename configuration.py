4bf3f704a849b1a637ab8e2aac651fd8e734f2d9
SergBugaenk0 <bugaenko.1976@yandex.ru>
2024-03-14 23:31:30 +0300

    init commit

--- configuration.py
+++ configuration.py
@@ -1,4 +1,3 @@
 URL_SERVICE = "https://0fe887c9-5b05-47c4-8bfd-c6c898dab0ba.serverhub.praktikum-services.ru"
-#CREATE_USER_PATH = "/api/v1/users/"
 CREATE_client_ORDER = "/api/v1/orders"
 GET_ORDER_on_TRACK = "/api/v1/orders/track"
